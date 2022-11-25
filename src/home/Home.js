cff1cdc0010ec0196db1f57b27cd62abf454f754
CHAN-HO <chanho2547@gmail.com>
2022-11-25 09:11:27 +0900

    찬호/11.26/기본프로젝트 업로드

---
+++ src/home/Home.js
@@ -0,0 +1,14 @@
+import FirstFloor from "../theaterInfo/charLotte/FirstFloor";
+import { Link } from "react-router-dom";
+
+
+const Home = () => {
+    return(
+        <>
+        <h1>Home 입니다</h1>
+        <Link to = "/FirstFloor"> 샤롯데 1층 정보 임시 연동</Link>
+        </>
+    );
+}
+
+export default Home;
