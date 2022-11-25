9b2e6842c633cc302a5e407b9dcab9f6348c9852
doyeonS2 <1162004@naver.com>
2022-11-25 16:11:12 +0900

    도연/11.25/Home.js 수정

--- src/home/Home.js
+++ src/home/Home.js
@@ -6,8 +6,10 @@ const Home = () => {
     return(
         <>
         <h1>Home 입니다</h1>
+        <Link to = "/login"> 로그인</Link> <br></br>
         <Link to = "/FirstFloor"> 샤롯데 1층 정보 임시 연동</Link> <br></br>
         <Link to = "/SecondFloor"> 샤롯데 2층 정보 임시 연동</Link>
+
         </>
     );
 }
