b4dcf32c07add49356eb579baeaa3c6ce1b25f98
doyeonS2 <1162004@naver.com>
2022-11-28 15:10:13 +0900

    도연/11.28/api수정

--- src/api/MuteApi.js
+++ src/api/MuteApi.js
@@ -5,16 +5,16 @@ const MUTE_DOMAIN = "http://localhost:8111/";
 const MuteApi =  {
 
     // 로그인
-    userLogin: async function(id, pwd) {
+    Login: async function(id, pwd) {
         const loginObj = {
-            user_id: id,
+            userId: id,
             pwd: pwd
         }
         return await axios.post(MUTE_DOMAIN + "/member/login", loginObj, HEADER);
     },
 
     // 아이디 찾기 
-    researchId: async function(name, mail) {
+    FindId: async function(name, mail) {
         const findIdObj = {
             name: name,
             mail: mail
