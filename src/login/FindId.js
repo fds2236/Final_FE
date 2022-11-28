acfb95647422dfab7ab53ac23ddd5bfed6ba3625
doyeonS2 <1162004@naver.com>
2022-11-28 14:14:39 +0900

    도연/11.28/아이디찾기 api 수정

--- src/login/FindId.js
+++ src/login/FindId.js
@@ -35,17 +35,9 @@ const FindId = () => {
             const res = await MuteApi.researchId(inputName, inputEmail);
             console.log(res.data.result);
             setModalOpen(true);
-            // localStorage에 JSON처리된 obj를 넣는다
-            // localStorage : 로컬 저장 공간에 접근할 수 있는 Storage 객체
-            // 저장한 데이터는 브라우저 세션 간에 공유됨
-            // localStorage의 데이터는 만료되지 않고 sessionStorage의 데이터는 페이지 세션이 끝날 때 사라짐
-            // setItem : 항목 추가
-            // getItem : 항목 읽기
-            // removeItem : 항목 제거
-            // clear : 항목 전제 제거
 
             if(res.data.result === "OK") {
-                setFindId(res.data.id); // 찍어주기위해 id값 저장
+                setFindId(res.data.id);
             } else {
                 setFindId("존재하는 아이디가 없습니다.");
             }
