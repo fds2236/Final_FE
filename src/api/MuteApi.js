c4702bc1fff94f2c97eb370c60875c759b2618a2
doyeonS2 <1162004@naver.com>
2022-11-29 17:49:50 +0900

    도연/11.29/로그인끝!!!!!!!!!!

--- src/api/MuteApi.js
+++ src/api/MuteApi.js
@@ -10,17 +10,9 @@ const MuteApi =  {
             userId: id,
             pwd: pwd
         }
-        return await axios.post(MUTE_DOMAIN + "/member/login", loginObj, HEADER);
-    },
-
-    // 아이디 찾기 
-    FindId: async function(name, mail) {
-        const findIdObj = {
-            name: name,
-            mail: mail
-        }
-        return await axios.post(MUTE_DOMAIN + "/member/findId", findIdObj, HEADER);
+        return await axios.post(MUTE_DOMAIN + "member/login", loginObj, HEADER);
     }
+
 
 }
 
--- src/login/Login.js
+++ src/login/Login.js
@@ -4,8 +4,6 @@ import MuteApi from "../api/MuteApi";
 import Modal from "../util/Modal";
 import styled from "styled-components";
 
-const LoginBox = styled.div``;
-
 const Login = () => {
        // 아이디, 비밀번호 입력
        const [inputId, setInputId] = useState("");
@@ -28,6 +26,8 @@ const Login = () => {
        const closeModal = () => {
            setModalOpen(false);
        };
+
+       let whoLogin = window.localStorage.getItem("whoLogin");
    
        // 아이디 힌트
        const onChangeId = (e) => {
@@ -58,20 +58,23 @@ const Login = () => {
         // API호출
        const onClickLogin = async() => {
         try {
-            const res = await MuteApi.userLogin(inputId, inputPwd);
-            console.log(res.data.result);
+            const res = await MuteApi.Login(inputId, inputPwd);
+            console.log(res.data);
 
-            if(res.data.result === 200) {
-                window.localStorage.setItem("whoLoginNow",inputId);
-                window.localStorage.setItem("whoPwdNow",inputPwd); 
+            if(res.data === 200) {
+                window.localStorage.setItem("whoLogin",inputId);
+                window.localStorage.setItem("whoPwd",inputPwd); 
+                console.log("로그인 성공");
                 window.location.replace("/");
 
-            } else if(res.data.result === 300) {
+            } else if(res.data === 300) {
+                setModelText("패스워드를 다시 확인해주세요.");
+                setModalOpen(true);
+                console.log("로그인 실패, 패스워드 불일치");
+            } else {
                 setModelText("존재하지 않는 아이디입니다.");
                 setModalOpen(true);
-            } else {
-                setModelText("패스워드를 다시 확인하여주시기 바랍니다.");
-                setModalOpen(true);
+                console.log("로그인 실패, 아이디가 없습니다.");
             }
         } catch (e) {
         } 
@@ -79,12 +82,13 @@ const Login = () => {
    
        return(
         <div className="container">
-            <LoginBox>
+            <>
+            <h3>현재 로그인 된 계정 [{whoLogin}]</h3>
                 <h5>로그인</h5>
                 {/* 아이디 입력창 */}
                 <input className="input" value={inputId} placeholder="아이디" onChange={onChangeId}></input>   
 
-                {/* 아이디 입력 제한 메시지 */}
+                {/* 아이디 제한 메시지 */}
                 <div className="hint">
                 {inputId.length > 0 && <span className={`message ${isId ? 'success' : 'error'}`}>{idMessage}</span>}
                 </div>
@@ -92,7 +96,7 @@ const Login = () => {
                 {/* 패스워드 입력창 */}
                 <input className="input" value={inputPwd} type="password" placeholder="비밀번호" onChange={onChangePwd}></input>
 
-                {/* 비밀번호 입력 제한 메시지 */}
+                {/* 비밀번호 제한 메시지 */}
                 <div className="hint">
                 {inputPwd.length > 0 && <span className={`message ${isPwd ? 'success' : 'error'}`}>{pwdMessage}</span>}
                 </div>
@@ -100,7 +104,7 @@ const Login = () => {
                 {/* 로그인 버튼 */}
                 <button className="loginButton" onClick={onClickLogin}>LOGIN</button>
                 <br/>
-            </LoginBox>
+            </>
 
             {modalOpen && <Modal open={modalOpen} close={closeModal} header="확인">{modalText}</Modal>}
         </div>
