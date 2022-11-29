bf823cf019685765a14bcb7cfa2a0997c4cab1c3
doyeonS2 <1162004@naver.com>
2022-11-29 17:51:36 +0900

    도연/11.29/로그인 끝!!!!!!!!!!!!!!!!!!

--- src/api/MuteApi.js
+++ src/api/MuteApi.js
@@ -3,7 +3,6 @@ const HEADER = "application/json";
 const MUTE_DOMAIN = "http://localhost:8282/";
 
 const MuteApi =  {
-
     // 로그인 - 도연
     Login: async function(id, pwd) {
         const loginObj = {
@@ -12,7 +11,6 @@ const MuteApi =  {
         }
         return await axios.post(MUTE_DOMAIN + "member/login", loginObj, HEADER);
     }
-
 
 }
 
