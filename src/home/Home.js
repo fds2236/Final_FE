3dfef981cfe743c4946aba708332060600a5b4ff
doyeonS2 <1162004@naver.com>
2022-11-30 15:51:28 +0900

    도연/11.30

--- src/home/Home.js
+++ src/home/Home.js
@@ -9,7 +9,6 @@ const Home = () => {
         <Link to = "/login"> 로그인</Link> <br></br>
         <Link to = "/FirstFloorChar"> 샤롯데 좌석 정보</Link> <br></br>
         <Link to = "/FirstFloorChung"> 충무아트 좌석 정보</Link> <br></br>
-        <Link to = "/MusicalReview"> ㅇㅇㅇㅇㅇ임시</Link> <br></br>
         <Link to = "/Reservation"> 예매 임시 페이지 </Link> <br></br>
         
 
