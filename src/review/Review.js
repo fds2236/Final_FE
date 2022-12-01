130804550ba0caf48f98194889f32de9faedd52c
doyeonS2 <1162004@naver.com>
2022-12-01 16:21:10 +0900

    도연/12.01/후기작성 별점추가

--- src/review/Review.js
+++ src/review/Review.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import MuteApi from "../api/MuteApi";
@@ -10,14 +10,23 @@ import ReviewModal from "../review/ReviewModal";
 // 후기 view - 도연 작업중
 
 const Review = () => {
+
+    useEffect(()=>{
+        window.localStorage.setItem("seatInfoMode","NONE");
+    })
+
     const [reviewMuId, setReviewMuId] = useState(''); // 뮤지컬 후기 글 번호
     const [musical, setMusical] = useState(''); // 공연 ID
     const [member, setMember] = useState(''); // 회원번호
     const [writeDate, setWriteDate] = useState(''); // 작성일
+
+    const [scoreTotal, setscoreTotal] = useState(''); // 별점 총점
+
     const [scoreStory, setScoreStory] = useState(''); // 스토리 별점
     const [scoreDirect, setScoreDirect] = useState(''); // 연출 별점
     const [scoreCast, setScoreCast] = useState(''); // 캐스팅 별점
     const [scoreNumber, setScoreNumber] = useState(''); // 넘버 별점
+
     const [reviewMuTxt, setReviewMuTxt] = useState(''); // 뮤지컬 후기 텍스트
 
     const [submit, setSubmit] = useState(false); // 서버로 전송할 수 있는 조건 체크
@@ -53,19 +62,12 @@ const Review = () => {
     }
 
 
-    // 후기 쓰기 취소 버튼
-    // const ReturnButton = (props) => {
-    //     return (
-    //         <>
-    //             <button onClick={OnClickToList} >{props.text}</button>
-    //         </>
-    //     );
-    // }
-
     const OnClickToList = () => {   
         window.location.replace('/Review');
     }
 
+    let array = [1, 2, 3 ,4]
+    
 
     // Api 호출
     // 후기 작성 버튼이 눌려지면 동작하는 함수
@@ -75,6 +77,11 @@ const Review = () => {
 
     const OnClickSeatWrite = () => {
         setSeModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setMuModalOpen(false);
+        setSeModalOpen(false);
     };
 
     const confirmModal = async() => {
@@ -87,13 +94,6 @@ const Review = () => {
         }
     };
 
-    const closeModal = () => {
-        setMuModalOpen(false);
-        setSeModalOpen(false);
-    };
-
-    
-
     return (
         <div className="container">
 
@@ -101,39 +101,41 @@ const Review = () => {
             <WriteTotalButton onClick={OnClickTotalWrite} text={"후기 작성"}></WriteTotalButton>
             {muModalOpen && <ReviewModal open={muModalOpen} confirm={confirmModal} close={closeModal} type={true} header="총평 후기">
             <div>
-                <div> 연출 <Rating /></div>
-                <div> 스토리 <Rating /></div>
-                <textarea placeholder="관람하신 뮤지컬의 총평을 작성해주세요."></textarea>
+                <fieldset>
+                    {/* 총점은 평균내서 기본적으로 색깔 나와있게 수정하기 !!! */}
+                    <div> 총점 <Rating/></div> 
+                    <div> 연출 <Rating/></div>
+                    <div> 스토리 <Rating/></div>
+                    <div> 캐스팅 <Rating/></div>
+                    <div> 넘버 <Rating/></div>
+                </fieldset>
+                <fieldset>
+                    <div>후기 작성</div>
+                    <textarea placeholder="관람하신 뮤지컬의 총평을 작성해주세요."></textarea>
+                </fieldset>
             </div>
             </ReviewModal>}
+            
+
 
             <div>좌석 후기</div>
             <WriteSeatButton onClick={OnClickSeatWrite} text={"후기 작성"}></WriteSeatButton>
             {seModalOpen && <ReviewModal open={seModalOpen} confirm={confirmModal} close={closeModal} type={true} header="좌석 후기">
             <div>
-                <div> 좌석
-                    <select>
-                    <option>층</option>
-                    <option></option>
-                    <option></option>
-                    </select>
-                    <select>
-                    <option>구역</option>
-                    <option></option>
-                    <option></option>
-                    </select>
-                    <select>
-                    <option>열</option>
-                    <option></option>
-                    <option></option>
-                    </select>
-                    <select>
-                    <option>번</option>
-                    <option></option>
-                    <option></option>
-                    </select>
-                </div>
-                <textarea placeholder="관람하신 좌석의 후기를 작성해주세요."></textarea>
+                <fieldset>
+                    {/* 총점은 평균내서 기본적으로 색깔 나와있게 수정하기 !!! */}
+                    <div> 총점 <Rating/></div> 
+                    <div> 좌석 <Rating/></div>
+                    <div> 시야 <Rating/></div>
+                    <div> 음향 <Rating/></div>
+                    <div> 조명 <Rating/></div>
+                </fieldset>
+                <fieldset>
+                    {/* 좌석은 아무거나 선택할 수 있음. 내 예매내역에 있는 좌석만 가져올 수 없을까?? */}
+                    <div> 좌석 <input / > <button>좌석 선택</button>
+                    </div>
+                    <textarea placeholder="관람하신 좌석의 후기를 작성해주세요."></textarea>
+                </fieldset>
             </div>
             </ReviewModal>}
 
